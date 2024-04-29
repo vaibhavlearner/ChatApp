5646b7ea389abec389fa19f11690da5e1a712db1
Vaibhav <vaibhavdma18@gmail.com>
2024-04-30 03:30:37 +0530

    done

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 const connectoMongoDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_DB_URI, {
-            useNewUrlParser: true,
-        });
+        await mongoose.connect(process.env.MONGO_DB_URI);
         console.log("Connected to MongoDB");
     } catch (error) {
         console.log("Error connecting to MongoDB", error.message);
